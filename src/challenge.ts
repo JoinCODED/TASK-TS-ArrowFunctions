6f39b0d214553b18885fb48fbcfc48d23c769bfa
Abdulrahman <AYAlkandari@pifss.gov.kw>
2025-03-09 12:48:51 +0300

    Heba, Abdulrahman

--- src/challenge.ts
+++ src/challenge.ts
@@ -3,6 +3,7 @@ import { square } from "./arrowFunctions";
 
 /**
  * 🌶️🌶️
+ * Heba, Abdulrahman
  * squares(numbers):
  * - Accepts "numbers" parameter of type "number[]"
  * - returns an array of numbers were each element of "numbers" argument is squared
@@ -12,10 +13,16 @@ import { square } from "./arrowFunctions";
  */
 
 // Comment out this function before starting your solution
-function squares(numbers: number[]): number[] {
-  return [0];
+//function squares(numbers: number[]): number[] {
+//  return [0];
+//}
+const squares = (numbers: number[]):number[] => {
+  let temp: number[] = []
+  for(let i=0; i<=numbers.length; i++){
+    temp.push(numbers[i]*numbers[i])
+  }
+  return temp
 }
-
 // example:
 squares([1, 2, 3]); // => [1, 4, 9]
 
